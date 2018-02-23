abef0af19c611c45f355d2d94f6729356f502d74
Kenneth Halim <kanisiuskenneth@gmail.com>
2018-02-24 01:14:58 +0700

    add main structure

---
+++ src/const/app.h
@@ -0,0 +1,9 @@
+#ifndef _APP_CONST_
+#define _APP_CONST_
+
+
+const int VIEWPORT_HEIGHT = 600;
+const int VIEWPORT_WIDTH = 800;
+
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <applib>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << VIEWPORT_HEIGHT << endl;
+}
